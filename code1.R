d7e0946419d91c36e104e084b07d4ff9cf5bd1f5
Sander Bossier <sander.bossier@outlook.com>
2020-04-14 14:37:57 +0200

    Committing my first R script to GitHub.

---
+++ code1.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(dslabs)
+
+# RStudio, Git, and GitHub - Key points
+
+# In terminal: configure git 
+  # git config --global user.name "Your Name"
+  # git config --global user.email "your@email.com"
+
+# In RStudio, create project > Version control > Git
+
+# Git pane: status symbols and color
+
+# Git actions:
+  # pull: pull changes from remote repo (if you are in collaboration with others)
+  # add: stage files for commit; click on stage box under git pane
+  # commit: commit to the local repo; click on "commit" button under git pane; add a commit message
+  # push: push to the remote repo on Github
+
+# .gitignore file: details check https://git-scm.com/docs/gitignore
